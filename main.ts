9ffd620771f3ab3327af428b6cb73e404785ecc4
log1342 <113910996+log1342@users.noreply.github.com>
2022-12-06 09:51:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("A C5 A C5 A C5 A C5 ", 201)
+    radio.sendString("EMERGENCY125")
+    music.playMelody("A C5 A C5 A C5 A C5 ", 201)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("A C5 A C5 A C5 A C5 ", 201)
+    radio.sendString("EMERGENCY126")
+    music.playMelody("A C5 A C5 A C5 A C5 ", 201)
+})
+radio.setGroup(125)
